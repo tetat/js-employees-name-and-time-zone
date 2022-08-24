e9e36a722d6f8e3b1fa6a5c0249a532e25e1ad20
tetat <nishat00101@gmail.com>
2022-08-24 18:48:23 +0600

    update time zone process

--- Time Zone/timezone.js
+++ Time Zone/timezone.js
@@ -1,21 +1,38 @@
 const container = document.getElementById('container');
 
-function setTime(cities) {
-    cities.map(city => {
-        const p = document.createElement('p');
-        p.innerText = `${city} 
-        Email received at 11:00 AM (on 25 August 2022)`;
-        container.appendChild(p);
+// function for find the specific country time zone
+const specificCountryTime = offset => {
+    let localDate = new Date();
+    let utc = localDate.getTime() + (localDate.getTimezoneOffset() * 60000);
+    let targetTime = new Date(utc + (3600000 * offset));
+    return [targetTime.toLocaleTimeString(), targetTime.toLocaleDateString()];
+}
+
+// check which of the employees have current time match with 11:00 AM (8/25/2020)
+// if match then sent the email
+function checkOnTime(employees) {
+    employees.map(employee => {
+        const [time, date] = specificCountryTime(parseInt(employee.offset));
+
+        if (date === "8/25/2022") {
+            const hourMinute = time.substring(0, 5);
+            const AM = time.substring(time.length - 2);
+
+            // console.log(hourMinute, AM, date);
+
+            if (hourMinute === "11:00" && AM == "AM") {
+                const p = document.createElement('p');
+                p.innerText = `at ${employee.abbreviation} ${hourMinute} ${AM} (${date})`;
+                container.appendChild(p);
+            }
+        }
     })
 }
 
-function getCityNames() {
-    const url = 'http://worldtimeapi.org/api/timezone/';
-    fetch(url)
+// load employees from store
+function loadEmployeesDetails() {
+    fetch('employees.json')
         .then(res => res.json())
-        .then(data => {
-            setTime(data);
-        });
+        .then(employees => checkOnTime(employees));
 }
-
-getCityNames();
+loadEmployeesDetails();
